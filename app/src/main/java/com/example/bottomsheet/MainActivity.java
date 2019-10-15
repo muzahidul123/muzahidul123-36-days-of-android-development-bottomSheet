dc15d31fe51451c2af0a698b5fd23e9d4c0a24ea
Muzahidul Islam <muzahidul123@gmail.com>
2019-10-15 21:23:42 +0600

    first commit

---
+++ app/src/main/java/com/example/bottomsheet/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.bottomsheet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FloatingActionButton fab;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        fab = findViewById(R.id.fab);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new BottomSheet().show(getSupportFragmentManager(), "bottom");
+            }
+        });
+    }
+}
